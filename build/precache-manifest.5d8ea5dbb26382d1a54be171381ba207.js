f014dd0eb1f35ed8e878d64a29d04233fd4ff750
vishalgunawad <vishalgunawad@gmail.com>
2022-11-17 19:24:07 +0530

    Add Build Folder for the Deploy

---
+++ build/precache-manifest.5d8ea5dbb26382d1a54be171381ba207.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe1db3d8ee59e0f58e2e",
+    "url": "/static/css/main.60155f41.chunk.css"
+  },
+  {
+    "revision": "fe1db3d8ee59e0f58e2e",
+    "url": "/static/js/main.fe1db3d8.chunk.js"
+  },
+  {
+    "revision": "7f8c91ddc4b58ecb5b87",
+    "url": "/static/js/1.7f8c91dd.chunk.js"
+  },
+  {
+    "revision": "e2b561d8fb25edfa2d40",
+    "url": "/static/js/2.e2b561d8.chunk.js"
+  },
+  {
+    "revision": "fa1e30c6e6c64559af32",
+    "url": "/static/js/runtime~main.fa1e30c6.js"
+  },
+  {
+    "revision": "736a42450bca14a7c272610f0b0c58c3",
+    "url": "/index.html"
+  }
+];
